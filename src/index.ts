533950c68a9b929208eeaa4c55ac050570cbb56c
Bloom <bloomdevelop@proton.me>
2025-01-10 12:54:57 -0400

    feat: add comments

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,13 @@ dotenv.config();
 
 const sql = neon(process.env.DATABASE_URL!);
 const db = drizzle({ client: sql, schema });
+
 const bot = new Client();
 
+// TODO: Add .env valiation (unecessary but important)
 bot.loginBot(process.env.TOKEN as string);
 
+// This whole fucking mess. I should've organized better
 bot.on("ready", () => {
   bot.servers.forEach(async (server) => {
     try {
@@ -20,6 +23,7 @@ bot.on("ready", () => {
           where: (users, { eq }) => eq(users.id, member.user?.id!!),
         });
 
+        // First, we check if there's a existing user in our database
         if (!existingUser) {
           const newUser: typeof schema.usersTable.$inferInsert = {
             id: member.user?.id!!,
@@ -37,8 +41,12 @@ bot.on("ready", () => {
             ),
           };
 
+          // Inserts these data into the database
           await db.insert(schema.usersTable).values(newUser);
         } else {
+
+          // If there's a user, you know what we do.
+          // Add a "pointless" warning (seriously?)
           console.warn(
             `User "${member.user?.username}" exist in the database!`
           );
@@ -49,6 +57,7 @@ bot.on("ready", () => {
         where: (servers, { eq }) => eq(servers.id, server.id),
       });
 
+      // Also it does applies to the servers part too
       if (!existingServer) {
         const newServer: typeof schema.serversTable.$inferInsert = {
           id: server.id,
@@ -75,18 +84,25 @@ bot.on("ready", () => {
         };
 
         await db.insert(schema.serversTable).values(newServer);
+        // Tell via console.log that we added it
         console.log("Saved into the database!\n", `Added server: ${server.id}`);
       } else {
+        // It does exactly the SAME THING, expect the prashe is different
         console.warn(`Server "${server.name}" already exists in database`);
         return;
       }
     } catch (error) {
+      // If there's a error, you're very much fucked up.
       console.error("HANDLED REJECTION:\n", error);
     }
   });
 });
 
 bot.on("messageCreate", async (msg) => {
+  // Every time a user/people sends a message, the bot will recieve "messageCreate" event
+  // with the content, even the whole message (expect only some needed values) data from the api/event.
+  // 
+  // Unsure if I need a timestamp here.
   const message: typeof schema.messagesTable.$inferInsert = {
     id: msg.id,
     data: JSON.stringify(
@@ -112,7 +128,7 @@ bot.on("messageCreate", async (msg) => {
   };
 
   await db.insert(schema.messagesTable).values(message);
-
+  // Tell that we added the exactly the whole damm thing in the database... Ugh...
   console.log(
     `New Message has been added into the database!\nMessage ID: ${msg.id}`
   );
